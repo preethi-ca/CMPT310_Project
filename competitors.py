0863d093de69018f6c461b9cb85738aa7c973593
global47 <romandeep04@icloud.com>
2026-08-04 13:59:31 -0700

    Added XGB

--- competitors.py
+++ competitors.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np
 from sklearn.neighbors import BallTree as bt
 
-
+DATA_PATH = "yelp-and-demo-info.csv"
 EARTH_RADIUS = 6371000  # in meters
 
 # load data from csv (latitude and longitude)
-comp_df = pd.read_csv("milestone1.csv").dropna(subset=["latitude", "longitude"])
+comp_df = pd.read_csv(DATA_PATH).dropna(subset=["latitude", "longitude"])
 if comp_df.empty:
 	raise ValueError(
-		f"milestone1.csv does not contain any valid latitude/longitude rows."
+		f"Data from {DATA_PATH} does not contain any valid latitude/longitude rows."
 	)
 
 transit_df = pd.read_csv("transit_stops.csv").dropna(subset=["latitude", "longitude"])
@@ -36,6 +36,6 @@ comp_df["competitor_count_500m"] = neighbors - 1 # subtract 1 to exclude the sto
 transit_distances_rad, _ = transit_tree.query(coords_rad, k=1)
 comp_df["nearest_transit_distance_m"] = transit_distances_rad[:, 0] * EARTH_RADIUS
 
-comp_df.to_csv("milestone1_with_competitors.csv", index=False)
+comp_df.to_csv(DATA_PATH, index=False)
 
-print("Competitor count and nearest transit stop distance calculated and saved to milestone1_with_competitors.csv")
+print(f"Competitor count and nearest transit stop distance calculated and saved to {DATA_PATH}")
