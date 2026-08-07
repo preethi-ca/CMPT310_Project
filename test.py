190846cfbeddde2cbb9b7556a1383104a86ae437
preethi chidambaram <preethi.ca@outlook.com>
2026-08-06 19:19:29 -0700

    changed success rating to 4.1

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ import pandas as pd
 # read the CSV
 businesses_df = pd.read_csv("550-info.csv")
 
-# redefine success using a 4.2 rating threshold
+# redefine success using a 4.1 rating threshold
 businesses_df["target_is_successful"] = (businesses_df["target_rating"] > 4.1).astype(int)
 
 # save the updated CSV
-businesses_df.to_csv("test-4.2.csv", index=False)
+businesses_df.to_csv("test-4.1.csv", index=False)
 
 # validate
 print(businesses_df["target_is_successful"].value_counts())
